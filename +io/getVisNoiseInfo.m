b6ff8f233931772ce4747a8d07353b194297770a
schroederlab <sylvia_schroeder@outlook.com>
2024-03-19 19:09:09 +0000

    Update to eye position code

--- +io/getVisNoiseInfo.m
+++ +io/getVisNoiseInfo.m
@@ -4,3 +4,4 @@ data.stimOrder = readNPY(fullfile(folder, '_ss_sparseNoise._ss_sparseNoiseID.npy
 data.times = readNPY(fullfile(folder, '_ss_sparseNoise.times.npy'));
 data.edges = readNPY(fullfile(folder, '_ss_sparseNoiseArea.edges.npy'));
 data.frames = readNPY(fullfile(folder, '_ss_sparseNoiseID.map.npy'));
+data.interval = readNPY(fullfile(folder, '_ss_recordings.sparseNoise_intervals.npy'));
--- main_eyePosInDegrees.m
+++ main_eyePosInDegrees.m
@@ -3,6 +3,8 @@
 minExplainedVarianceStim = 0.01;
 minPVal = 0.05;
 maxLambda = 1;
+% for binning of horizontal eye position
+numEyeBins = 10;
 
 %% Fit RF to each eye position for cells with good overall RFs
 % Note: for all SC neurons recorded with 2P, recording was in right
@@ -25,10 +27,22 @@ for subj = 1:length(subjects)
         noiseData = io.getVisNoiseInfo(folder);
         rfData = io.getRFData(folder);
 
+        % interpolate pupil data to match stimulus times
+
+        % smooth pupil data! (unless already done)
+
+        t_stim = noiseData.times;
+        eyePos = interp1(pupilData.time, pupilData.pos(:,1), t_stim);
+
         % percentiles of horizontal eye positions
         % Note: psition from nasal (smaller) to temporal (larger)
-        eyePosPerc = prctile(pupilData.pos(:,1), 5:10:95) - ...
-            median(pupilData.pos(:,1), 'omitnan');
+        eyePosQuart = prctile(pupilData.pos(:,1), [25 50 75]);
+        eyePosNorm = (pupilData.pos(:,1) - eyePosQuart(2)) ./ ...
+            diff(eyePosQuart([1 3]));
+        eyeStart = find(pupilData.time >= noiseData.interval(1), 1);
+        eyeEnd = find(pupilData.time > noiseData.interval(2), 1) - 1;
+        [~,eyeBinEdges,bin] = histcounts(eyePosNorm(eyeStart:eyeEnd), ...
+            prctile(eyePosNorm(eyeStart:eyeEnd), 0:numEyeBins:100));
 
         validRF = find(rfData.pValues < minPVal & ...
             rfData.explVars > minExplainedVarianceStim & ...
