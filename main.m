8c250cf6589f7f7912b9f91b81637f59bc327d71
Federico Rossi <luigi.federico.rossi@gmail.com>
2024-04-18 18:28:35 +0200

    revert to last Federico s commit

--- main.m
+++ main.m
@@ -9,12 +9,21 @@ clear;
 % folder.codeToolboxes = 'C:\dev\toolboxes';
 % folder.codeThis = 'C:\dev\workspaces';
 
-% folders Federico
-folder.data = '/Users/federico/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Neural correlates of eye movements/Paper/SC_data';
-folder.results = '/Users/federico/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Neural correlates of eye movements/Paper/Results';
-folder.plots = '/Users/federico/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Neural correlates of eye movements/Paper/Plots';
-folder.codeToolboxes = '/Users/federico/Documents/GitHub';
-folder.codeThis = '/Users/federico/Documents/GitHub';
+% % folders Federico Laptop
+% folder.data = '/Users/federico/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Neural correlates of eye movements/Paper/SC_data';
+% folder.results = '/Users/federico/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Neural correlates of eye movements/Paper/Results';
+% folder.plots = '/Users/federico/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Neural correlates of eye movements/Paper/Plots';
+% folder.codeToolboxes = '/Users/federico/Documents/GitHub';
+% folder.codeThis = '/Users/federico/Documents/GitHub';
+% 
+
+% folders Federico's WS
+folder.data = 'D:\OneDrive - University College London\Neural correlates of eye movements\Paper\SC_data';
+folder.results = 'D:\OneDrive - University College London\Neural correlates of eye movements\Paper\Results';
+folder.plots = 'D:\OneDrive - University College London\Neural correlates of eye movements\Paper\Plots';
+folder.codeToolboxes = 'D:\OneDrive - Fondazione Istituto Italiano Tecnologia\Documents\Code\Stable';
+folder.codeThis = 'D:\OneDrive - Fondazione Istituto Italiano Tecnologia\Documents\Code\Dev';
+
 
 %% Add paths
 addpath(genpath(fullfile(folder.codeToolboxes, 'npy-matlab')))
@@ -22,10 +31,10 @@ addpath(genpath(fullfile(folder.codeThis, 'schwartzlose_sit_et_al-saccades_SC'))
 addpath(genpath(fullfile(folder.codeToolboxes, 'FedBox')))
 
 %% Map eye position in pixels to eye position in visual degrees (Sylvia)
-% determineEyePosInDegrees(folder);
+determineEyePosInDegrees(folder);
 
 %% Saccade dynamics (Heather)
 
 
 %% Saccade triggered responses (Federico)
-main_saccade_resp;
+main_saccade_resp(folder);
